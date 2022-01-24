2a32d8808e7f4d4d18b4288d669c51d84e9115c1
weiher <stefan.weiher@eurecom.fr>
2022-01-24 17:19:04 +0100

    works so far

--- app.js
+++ app.js
@@ -7,18 +7,47 @@ var audio_player = new Vue({
     }
 })
 
-var audio_input = new Vue({
-    el: '#audio_input',
+var audio_search = new Vue({
+    el: '#audio_search',
     data: {
         msg: ''
     },
     methods: {
         onEnter: function () {
-            fetchWord(audio_input.msg);
-            audio_input.msg = ""
+            fetchWord(audio_search.msg);
         }
     }
 });
+
+var audio_recorder = new Vue({
+    el: '#audio_recorder',
+    data: {
+        audio: ''
+    },
+
+});
+
+var record_button = new Vue({
+    el: '#record_button',
+    methods: {
+        onClick: function () {
+            recordAudio()
+        }
+    }
+});
+
+var pass_button = new Vue({
+    el: '#pass_button',
+    data: {
+        disabled: true
+    },
+    methods: {
+        onClick: function () {
+            fetchWord("Pass")
+        }
+    }
+});
+
 
 async function fetchWord(word) {
     const response = await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word);
@@ -30,7 +59,9 @@ async function fetchWord(word) {
     audio_player.phonetic_script = data[0]["phonetics"][0]["text"];
     audio_player.audio = data[0]["phonetics"][0]["audio"];
 
-    console.log(audio_player.audio)
+    audio_search.url = ""
+    audio_recorder.audio = ""
+    pass_button.disabled = true
 }
 
 
@@ -50,4 +81,31 @@ async function fetchAllWords(all_words = false) {
 }
 
 fetchAllWords()
+
+
+async function recordAudio() {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    const audioChunks = [];
+    const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+
+    mediaRecorder.addEventListener("dataavailable", event => {
+        audioChunks.push(event.data);
+    });
+    mediaRecorder.addEventListener("stop", () => {
+        const audioBlob = new Blob(audioChunks);
+        const audioUrl = URL.createObjectURL(audioBlob);
+        audio_recorder.audio = audioUrl
+
+    });
+    mediaRecorder.start()
+    await sleep(2000);
+    mediaRecorder.stop()
+    pass_button.disabled = false
+}
+
+
+
+
+
 
