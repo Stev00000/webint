cd590e227ca3c019829b7f7660038e80b7dad29a
weiher <stefan.weiher@eurecom.fr>
2022-01-25 00:52:38 +0100

    allow login and logout

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var current_user = null
+var current_lections = null
+var current_number = null
+var increase_count = false
+
 var audio_player = new Vue({
     el: "#audio_player",
     data: {
@@ -10,14 +15,51 @@ var audio_player = new Vue({
 var audio_search = new Vue({
     el: '#audio_search',
     data: {
-        msg: ''
+        input: ''
     },
     methods: {
         onEnter: function () {
-            fetchWord(audio_search.msg);
+            fetchWord(audio_search.input);
+            increase_count = false
         }
     }
 });
+
+var side_area = new Vue({
+    el: '.side_area',
+    data: {
+        input: '',
+        hide_logout: true,
+        hide_register: false
+
+    },
+    methods: {
+        clickRegister: function () {
+            current_user = side_area.input
+            if (localStorage.getItem(current_user + "_lections") === null) {
+                fetchAllWords(current_user)
+            } else {
+                current_lections = JSON.parse(localStorage.getItem(current_user + "_lections"))
+                current_number = localStorage.getItem(current_user + "_number")
+            }
+            after_login_area.hidden = false
+            side_area.hide_logout = false
+            side_area.hide_register = true
+            side_area.input = ""
+            increase_count = false
+        },
+        clickLogout: function () {
+            after_login_area.hidden = true
+            side_area.hide_logout = true
+            side_area.hide_register = false
+            current_user = null
+            current_lections = null
+            current_number = null
+        }
+    }
+});
+
+
 
 var audio_recorder = new Vue({
     el: '#audio_recorder',
@@ -36,52 +78,73 @@ var record_button = new Vue({
     }
 });
 
-var pass_button = new Vue({
-    el: '#pass_button',
+var after_login_area = new Vue({
+    el: '.after_login_area',
     data: {
-        disabled: true
+        disabled: true,
+        hidden: true
     },
     methods: {
         onClick: function () {
-            fetchWord("Pass")
+            console.log(current_number + " " + increase_count)
+
+            if (increase_count) {
+                current_number++
+                localStorage.setItem(current_user + "_number", current_number)
+                fetchWord(current_lections[current_number])
+            } else {
+                fetchWord(current_lections[current_number])
+                increase_count = true
+            }
+
         }
     }
 });
 
 
 async function fetchWord(word) {
-    const response = await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word);
-    const data = await response.json()
-    // waits until the request completes...
-    //const response = await JSON.parse(response_json)
+    try {
+        console.log("fetch word: " + word)
+        const response = await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word);
+        const data = await response.json()
+        // waits until the request completes...
+        //const response = await JSON.parse(response_json)
 
-    audio_player.word = data[0]["word"]
-    audio_player.phonetic_script = data[0]["phonetics"][0]["text"];
-    audio_player.audio = data[0]["phonetics"][0]["audio"];
+        audio_player.word = data[0]["word"]
+        audio_player.phonetic_script = data[0]["phonetics"][0]["text"];
+        audio_player.audio = data[0]["phonetics"][0]["audio"];
 
-    audio_search.url = ""
-    audio_recorder.audio = ""
-    pass_button.disabled = true
-}
-
-
-async function fetchAllWords(all_words = false) {
-    if (all_words) {
-        var response = await fetch("all_words.txt")
-    } else {
-        var response = await fetch("important_words.txt")
+        audio_search.input = ""
+        audio_recorder.audio = ""
+        after_login_area.disabled = true
+    } catch (e) {
+        console.error("Error occured while fetching");
+        console.error(e);
     }
 
-    const data = await response.text()
-    complete_word_array = data.split("\n")
-    console.log(complete_word_array.length)
-    // waits until the request completes...
-    //const response = await JSON.parse(response_json)
-
 }
 
-fetchAllWords()
 
+async function fetchAllWords(email) {
+    const response = await fetch("important_words.txt")
+    const data = await response.text()
+    complete_word_array = data.split("\r\n")
+
+    current_lections = await complete_word_array.sort(() => 0.5 - Math.random())
+    current_number = 0
+
+    localStorage.setItem(email + "_lections", JSON.stringify(current_lections));
+    localStorage.setItem(email + "_number", 0);
+}
+
+async function getLection() {
+    const response = await fetch("important_words.txt")
+    const data = await response.text()
+    complete_word_array = data.split("\r\n")
+    const shuffled = complete_word_array.sort(() => 0.5 - Math.random());
+    var selected = shuffled.slice(0, 10);
+    return selected
+}
 
 async function recordAudio() {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -101,11 +164,25 @@ async function recordAudio() {
     mediaRecorder.start()
     await sleep(2000);
     mediaRecorder.stop()
-    pass_button.disabled = false
+    after_login_area.disabled = false
 }
 
 
+function empty_storage() {
+    localStorage.clear()
+}
 
 
+function allStorage() {
 
+    var archive = [],
+        keys = Object.keys(localStorage),
+        i = 0, key;
+
+    for (; key = keys[i]; i++) {
+        archive.push(key + '=' + localStorage.getItem(key));
+    }
+
+    return archive;
+}
 
